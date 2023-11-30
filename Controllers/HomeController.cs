a6e605c51f0c4d5f44e77b175c0d4e8a67b2b961
Cylas Kiganda <kigandacylas@gmail.com>
2023-11-30 20:30:09 +0300

    HttpGet and HttpPost actions created

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
 namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
@@ -9,7 +11,14 @@ namespace PartyInvites.Controllers
             ViewBag.Greeting = hour < 12 ? "Good morning" : "Good Afternoon";   
             return View("MyView");
         }
+
+        [HttpGet]
         public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
         {
             return View();
         }
